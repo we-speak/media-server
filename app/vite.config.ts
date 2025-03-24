16eabd23e7264e0ada601296f3d70b9db5ce70ef
Iñaki Baz Castillo <ibc@aliax.net>
2025-03-24 13:04:25 +0100

    cosmetic

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,31 +1,40 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-
-// try to get host domain from server/config.js if any
-// also get https config
 import Module from 'node:module';
 import path from 'node:path';
 import fs from 'node:fs';
 import url from 'node:url';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import qs from 'qs';
+import waitPort from 'wait-port';
+import openBrowser from 'react-dev-utils/openBrowser';
+
 const require = Module.createRequire(import.meta.url);
+
 let host = process.env.DOMAIN || 'localhost';
 let cert: Buffer<ArrayBufferLike> | undefined = undefined;
 let key: Buffer<ArrayBufferLike> | undefined = undefined;
+
+// Try to read server/config.js.
 try {
   const c = require('../server/config');
+
   host = c.domain || host;
   cert = readTls(c.https.tls?.cert);
   key = readTls(c.https.tls?.key);
-} catch (err) {
+} catch (error) {
   // ignore file not found
 }
+
 const port = Number(process.env.VITE_LISTEN_PORT) || 3000;
 const https = cert && key ? { cert, key } : undefined;
+const alias: { [k: string]: string } = {};
 
-const alias: {[k: string]: string} = {}
+// Use mediasoup-client from same folder to allow hot reload when changing code
+// in mediasoup-client.
 if (process.env.LOCAL) {
-  const localClientEntryPoint = '../../mediasoup-client/src/index.ts'
-  alias['mediasoup-client'] = path.join(__dirname, localClientEntryPoint)
+  const localClientEntryPoint = '../../mediasoup-client/src/index.ts';
+
+  alias['mediasoup-client'] = path.join(__dirname, localClientEntryPoint);
 }
 
 export default defineConfig({
@@ -43,14 +52,11 @@ export default defineConfig({
   },
 });
 
-import qs from 'qs';
-import openBrowser from 'react-dev-utils/openBrowser';
-import waitPort from 'wait-port';
-
 runDev();
 
 async function runDev() {
   const dev = process.env.DEV;
+
   if (!dev) {
     return;
   }
@@ -66,9 +72,11 @@ async function runDev() {
     _throttleSecret: 'foo',
     info: true,
   };
+
   const defaultProducer = {
     consume: false,
   };
+
   const defaultConsumer = {
     produce: false,
   };
@@ -76,44 +84,64 @@ async function runDev() {
   let producer = null;
   let consumer = null;
 
-  if (dev === 'tcp') {
-    producer = {
-      roomId: 'dev-tcp',
-      forceTcp: true,
-    };
-    consumer = {
-      roomId: 'dev-tcp',
-      forceTcp: true,
-    };
-  } else if (dev === 'vp9') {
-    producer = {
-      roomId: 'dev-vp9',
-      forceVP9: true,
-      numSimulcastStreams: 3,
-      webcamScalabilityMode: 'L1T3',
-    };
-    consumer = {
-      roomId: 'dev-vp9',
-      forceVP9: true,
-    };
-  } else if (dev === 'h264') {
-    producer = {
-      roomId: 'dev-h264',
-      forceH264: true,
-    };
-    consumer = {
-      roomId: 'dev-h264',
-      forceH264: true,
-    };
-  } else if (dev === 'av1') {
-    producer = {
-      roomId: 'dev-av1',
-      forceAV1: true,
-    };
-    consumer = {
-      roomId: 'dev-av1',
-      forceAV1: true,
-    };
+  switch (dev) {
+    case 'tcp': {
+      producer = {
+        roomId: 'dev-tcp',
+        forceTcp: true,
+      };
+
+      consumer = {
+        roomId: 'dev-tcp',
+        forceTcp: true,
+      };
+
+      break;
+    }
+
+    case 'vp9': {
+      producer = {
+        roomId: 'dev-vp9',
+        forceVP9: true,
+        numSimulcastStreams: 3,
+        webcamScalabilityMode: 'L1T3',
+      };
+
+      consumer = {
+        roomId: 'dev-vp9',
+        forceVP9: true,
+      };
+
+      break;
+    }
+
+    case 'h264': {
+      producer = {
+        roomId: 'dev-h264',
+        forceH264: true,
+      };
+
+      consumer = {
+        roomId: 'dev-h264',
+        forceH264: true,
+      };
+
+      break;
+    }
+
+    case 'av1': {
+      producer = {
+        roomId: 'dev-av1',
+        forceAV1: true,
+      };
+
+      consumer = {
+        roomId: 'dev-av1',
+        forceAV1: true,
+      };
+
+      break;
+    }
   }
 
   const qProducer = qs.stringify({
@@ -121,6 +149,7 @@ async function runDev() {
     ...defaultProducer,
     ...producer,
   });
+
   open(qProducer);
 
   const qConsumer = qs.stringify({
@@ -128,12 +157,14 @@ async function runDev() {
     ...defaultConsumer,
     ...consumer,
   });
+
   open(qConsumer);
 }
 
 function open(query: string) {
   const protocol = https ? 'https' : 'http';
   const url = `${protocol}://${host}:${port}/?${query}`;
+
   openBrowser(url);
 }
 
@@ -141,13 +172,16 @@ function readTls(v: string) {
   if (!v) {
     return;
   }
+
   if (!path.isAbsolute(v)) {
     const dir = path.dirname(url.fileURLToPath(import.meta.url));
+
     v = path.join(dir, '../server', v);
   }
+
   try {
     return fs.readFileSync(v);
-  } catch (err) {
-    // ignore file not found
+  } catch (error) {
+    // Ignore file not found.
   }
 }
